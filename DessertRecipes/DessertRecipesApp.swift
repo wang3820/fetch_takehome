e99f75b925a597505a9fa04292ded783a5b77695
Tong Wang <wtrayraca@hotmail.com>
2024-06-12 12:38:26 -0500

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Ray on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
