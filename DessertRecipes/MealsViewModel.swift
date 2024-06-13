53142793df48540d737b419afd617d71cb4cdb05
Tong Wang <wtrayraca@hotmail.com>
2024-06-13 09:33:35 -0500

    Sorted meals

--- DessertRecipes/MealsViewModel.swift
+++ DessertRecipes/MealsViewModel.swift
@@ -8,6 +8,40 @@
 
 import Foundation
 
+extension Meal: Comparable {
+    static func == (lhs: Meal, rhs: Meal) -> Bool {
+        if let leftName = lhs.strMeal, let rightName = rhs.strMeal {
+            return leftName == rightName
+        }
+        else {
+            guard lhs.strMeal != nil else {
+                guard rhs.strMeal != nil else {
+                    return true
+                }
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    static func < (lhs: Meal, rhs: Meal) -> Bool {
+        if let leftName = lhs.strMeal, let rightName = rhs.strMeal {
+            return leftName < rightName
+        }
+        else {
+            guard lhs.strMeal != nil else {
+                guard rhs.strMeal != nil else {
+                    return true
+                }
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
 @MainActor class MealsViewModel: ObservableObject {
     @Published var meals = [Meal]()
     
@@ -15,7 +49,7 @@ import Foundation
         guard let fetchedMeals: MealResponse = await NetworkService(requestUrl: url).retriveData() else {
             return
         }
-        meals = fetchedMeals.meals
+        meals = fetchedMeals.meals.sorted(by: <)
     }
     
     func mealIsValid(meal: Meal) -> Bool {
